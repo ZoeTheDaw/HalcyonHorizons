5f7729f0ed90071918b5c07e0673ee0c0926266b
meepanish <meepanish@gmail.com>
2025-04-04 07:38:26 +0800

    Just commente out code, can be easily uncommented

--- src/main/java/com/internals/halcyonhorizons/server/entity/HorizonsEntityRegistry.java
+++ src/main/java/com/internals/halcyonhorizons/server/entity/HorizonsEntityRegistry.java
@@ -1,14 +1,14 @@
-package com.internals.halcyonhorizons.server.entity;
-
-import com.internals.halcyonhorizons.HalcyonHorizons;
-import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.MobCategory;
-import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegistryObject;
-
-public class HorizonsEntityRegistry {
-    public static final DeferredRegister<EntityType<?>> DEF_REG = DeferredRegister.create(ForgeRegistries.ENTITY_TYPES, HalcyonHorizons.MODID);
-    public static final MobCategory SKY_CREATURE = MobCategory.create("sky_creature", "halcyonhorizons:sky_creature", 10, true, true, 128);
-    public static final RegistryObject<EntityType<RuffianEntity>> RUFFIAN = DEF_REG.register("ruffian", () -> (EntityType) EntityType.Builder.of(RuffianEntity::new, SKY_CREATURE).sized(2.3F, 2.5F).setTrackingRange(8).build("ruffian"));
-}
+//package com.internals.halcyonhorizons.server.entity;
+//
+//import com.internals.halcyonhorizons.HalcyonHorizons;
+//import net.minecraft.world.entity.EntityType;
+//import net.minecraft.world.entity.MobCategory;
+//import net.minecraftforge.registries.DeferredRegister;
+//import net.minecraftforge.registries.ForgeRegistries;
+//import net.minecraftforge.registries.RegistryObject;
+//
+//public class HorizonsEntityRegistry {
+//    public static final DeferredRegister<EntityType<?>> DEF_REG = DeferredRegister.create(ForgeRegistries.ENTITY_TYPES, HalcyonHorizons.MODID);
+//    public static final MobCategory SKY_CREATURE = MobCategory.create("sky_creature", "halcyonhorizons:sky_creature", 10, true, true, 128);
+//    public static final RegistryObject<EntityType<RuffianEntity>> RUFFIAN = DEF_REG.register("ruffian", () -> (EntityType) EntityType.Builder.of(RuffianEntity::new, SKY_CREATURE).sized(2.3F, 2.5F).setTrackingRange(8).build("ruffian"));
+//}
--- src/main/java/com/internals/halcyonhorizons/server/entity/util/RuffianReaction.java
+++ src/main/java/com/internals/halcyonhorizons/server/entity/util/RuffianReaction.java
@@ -1,50 +1,50 @@
-package com.internals.halcyonhorizons.server.entity.util;
-
-import com.internals.halcyonhorizons.server.entity.living.RuffianBaseEntity;
-import net.minecraft.world.entity.player.Player;
-
-public enum RuffianReaction {
-    WARY(0, 80),
-    AGGRESSIVE(0, 40),
-    NEUTRAL(10, 25),
-    HELPFUL(8, 30);
-
-    private double minDistance;
-    private double maxDistance;
-
-    RuffianReaction(double minDistance, double maxDistance) {
-        this.minDistance = minDistance;
-        this.maxDistance = maxDistance;
-    }
-
-    public double getMinDistance() {
-        return this.minDistance;
-    }
-
-    public double getMaxDistance() {
-        return this.maxDistance;
-    }
-
-    public static RuffianReaction fromReputation(int rep) {
-        if (rep <= -10) {
-            return AGGRESSIVE;
-        }
-        if (rep <= 10) {
-            return WARY;
-        }
-        if (rep <= 30) {
-            return NEUTRAL;
-        }
-        return HELPFUL;
-    }
-
-    public boolean validPlayer(RuffianBaseEntity ruffian, Player player) {
-        if (this == WARY && player.getY() > ruffian.getY() + 15) {
-            return false;
-        }
-        if (this != AGGRESSIVE && this != HELPFUL) {
-            return player.isInWaterOrBubble() || !ruffian.isInWaterOrBubble();
-        }
-        return true;
-    }
-}
+//package com.internals.halcyonhorizons.server.entity.util;
+//
+//import com.internals.halcyonhorizons.server.entity.living.RuffianBaseEntity;
+//import net.minecraft.world.entity.player.Player;
+//
+//public enum RuffianReaction {
+//    WARY(0, 80),
+//    AGGRESSIVE(0, 40),
+//    NEUTRAL(10, 25),
+//    HELPFUL(8, 30);
+//
+//    private double minDistance;
+//    private double maxDistance;
+//
+//    RuffianReaction(double minDistance, double maxDistance) {
+//        this.minDistance = minDistance;
+//        this.maxDistance = maxDistance;
+//    }
+//
+//    public double getMinDistance() {
+//        return this.minDistance;
+//    }
+//
+//    public double getMaxDistance() {
+//        return this.maxDistance;
+//    }
+//
+//    public static RuffianReaction fromReputation(int rep) {
+//        if (rep <= -10) {
+//            return AGGRESSIVE;
+//        }
+//        if (rep <= 10) {
+//            return WARY;
+//        }
+//        if (rep <= 30) {
+//            return NEUTRAL;
+//        }
+//        return HELPFUL;
+//    }
+//
+//    public boolean validPlayer(RuffianBaseEntity ruffian, Player player) {
+//        if (this == WARY && player.getY() > ruffian.getY() + 15) {
+//            return false;
+//        }
+//        if (this != AGGRESSIVE && this != HELPFUL) {
+//            return player.isInWaterOrBubble() || !ruffian.isInWaterOrBubble();
+//        }
+//        return true;
+//    }
+//}
